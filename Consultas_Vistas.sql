f3299f5b63bd20b96e839f682a7c9a0615dd8761
YermaniRo <msmdh@gmail.com>
2025-06-16 18:20:56 -0500

    Segundo Commit

---
+++ Consultas_Vistas.sql
@@ -0,0 +1,5 @@
+SELECT * FROM vista_pedidos;
+SELECT * FROM vista_productos_stock;
+SELECT * FROM vista_clientes_frecuentes;
+SELECT * FROM vista_productos_mas_vendidos;
+
---
+++ EjemploDePedido.sql
@@ -0,0 +1,3 @@
+CALL realizar_pedido(1, 1, 1); -- Cliente 1 compra 1 Laptop
+CALL realizar_pedido(1, 2, 2); -- Cliente 1 compra 2 Mouse
+CALL realizar_pedido(2, 1, 1); -- Cliente 2 compra 1 Laptop
